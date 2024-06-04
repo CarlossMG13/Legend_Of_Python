13440f9ef1cfcb046c23ef0242c7b5f223b57eab
Carlos Morales García <116101975+CarlossMG13@users.noreply.github.com>
2024-06-04 15:15:44 -0600

    Add files via upload

---
+++ PyDesde0/25_reading-list.py
@@ -0,0 +1,8 @@
+books = ['Zero to One', 'The Lean Startup', 'The Mom Test', 'Make it stick', 'Life in Code']
+
+books.append('Zero to Sold')
+print(books)
+
+books.remove('The Lean Startup')
+books.pop(0)
+print(books)
---
+++ PyDesde0/26_mixtape.py
@@ -0,0 +1,16 @@
+# 💿 Theme: Electronic Music
+
+playlist = [
+  'Rumble - Skrillex',
+  'You & me - Disclosure',
+  'Desire - Years and years',
+  'Overthinker - INZO',
+  'Hold Me - Lavern',
+  'EN MI MENTE SOLO TU - Mario Santander'
+]
+
+for i in playlist:
+    print(i)
+
+for i in range(len(playlist)):
+    print(playlist[i])
