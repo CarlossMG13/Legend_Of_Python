aacb673aef2f15cbb09d91a9c925cf27a0ff06e5
Carlos Morales García <116101975+CarlossMG13@users.noreply.github.com>
2024-06-05 14:49:24 -0600

    Add files via upload

---
+++ PyDesde0/28_dry.py
@@ -0,0 +1,15 @@
+# Show the content in the terminal
+print('Hello world')
+
+#List of prices
+prices = [123, 234, 567, 789]
+#Print the lenght of the list
+print(len(prices))
+#Print the maximum value
+print(max(prices))
+#Print the minimum value
+print(min(prices))
+
+#New built-it function 
+#Converts the str into a float 
+print(float('1.11'))
---
+++ PyDesde0/31_calculator.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def exp(a, b):
+    return a ** b
+
+print(add(3, 5))
+
+print(subtract(10, 5))
+
+print(multiply(2, 10))
+
+print(divide(10, 2))
+
+print(exp(10, 2))
